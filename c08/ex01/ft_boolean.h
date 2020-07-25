4add65ed12ed99f0c5e9fe084c31b0f4c3058d55
Yury <i@LAPTOP-SCN2A1DL.localdomain>
2020-07-25 15:02:30 +0300

    ex02 done

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,12 @@
+#ifndef __FT_BOOLEAN_H
+#define __FT_BOOLEAN_H
+#define t_bool int
+#define TRUE 1
+#define FALSE 0
+#define EVEN(a) a % 2 == 0
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define SUCCESS 1
+#include <unistd.h>
+#endif
+

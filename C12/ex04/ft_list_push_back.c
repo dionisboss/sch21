00672f7189a58e0ff1b506540d3c54241c0dc247
Yury <i@LAPTOP-SCN2A1DL.localdomain>
2020-07-30 19:19:20 +0300

    ex04

---
+++ C12/ex04/ft_list_push_back.c
@@ -0,0 +1,16 @@
+
+#include "ft_list.h"
+
+void ft_list_push_back(t_list **begin_list, void *data)
+{
+	t_list *tmp;
+
+	tmp = ft_create_elem(data);
+
+	while((*begin_list) != NULL)
+	{
+		(*begin_list) = (*begin_list)->next;
+	}
+	(*begin_list) = tmp;
+}
+

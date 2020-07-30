76428c249b4534e1136c7af25b71b53967f969ab
Yury <i@LAPTOP-SCN2A1DL.localdomain>
2020-07-30 20:58:17 +0300

    minor eddits

--- C12/ex04/ft_list_push_back.c
+++ C12/ex04/ft_list_push_back.c
@@ -5,14 +5,14 @@ void ft_list_push_back(t_list **begin_list, void *data)
 {
 	t_list *tmp;
 
-	if ((*begin_list) != NULL)
+	if ((*begin_list)= NULL)
 	{
 		tmp = ft_create_elem(data);
-		while((*begin_list) != NULL)
+		while((*begin_list)->next != NULL)
 		{
 			(*begin_list) = (*begin_list)->next;
 		}
-		(*begin_list) = tmp;
+		(*begin_list)->next = tmp;
 	}
 	else 
 		(*begin_list) = ft_create_elem(data);
